528e36196accb2434e37dadb3f8971c7a1a1f1df
Rahul ray <102358586+mrrahulray@users.noreply.github.com>
2024-07-02 02:38:12 +0530

    version 2.0

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,7 +1,4 @@
-# Simple BMI calculator
-#description:
-# BMI= Body Mass Index
-# formula = (weight/(height^2))
+#version 2.0
 print("Welcomto BMI calculator!")
 name=input("What is your name:")
 Height = float(input("Enter the height (miter): "))
@@ -11,4 +8,15 @@ print(f"{name}, your Height is {Height} miter and weight is {int(Weight)} kg")
 
 
 BMI = Weight/Height**2
-print(f"Your BODY mask index (BMI) is {int(BMI)}")
+
+
+if (BMI<18.5):
+    print(f"Your BMI is {BMI}, you are Underweight")
+elif (BMI>=18.5 and BMI<25):
+    print(f"Your BMI is {BMI}, you have a normal weight")
+elif (BMI>=25 and BMI<30):
+    print(f"Your BMI is {BMI}, you are slightly overweight")
+elif (BMI>=30 and BMI<35):
+    print(f"Your BMI is {BMI}, you are obese")
+elif (BMI>=35):
+    print(f"Your BMI is {BMI}, you are clinically obese")
